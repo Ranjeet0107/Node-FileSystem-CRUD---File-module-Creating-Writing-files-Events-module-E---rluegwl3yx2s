e08b8f9b60cbdc2bf61e7b91b7c25e40ec0083f7
Ranjeet <ranjeet.ggi@gmail.com>
2023-03-31 16:52:24 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 const fs = require('fs/promises')
 
 const myFileWriter = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
+  try {
+    await fs.writeFile(fileName, fileContent)
+    console.log(`File ${fileName} has been created.`)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 const myFileReader = async (fileName) => {
-	// write code here
-	// dont chnage function name
+  try {
+    const fileContent = await fs.readFile(fileName, 'utf-8')
+    console.log(`File ${fileName} has been read.`)
+    return fileContent
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-
 const myFileUpdater = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
+  try {
+    const existingContent = await myFileReader(fileName)
+    await fs.writeFile(fileName, existingContent + fileContent)
+    console.log(`File ${fileName} has been updated.`)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 const myFileDeleter = async (fileName) => {
-	// write code here
-	// dont chnage function name
+  try {
+    await fs.unlink(fileName)
+    console.log(`File ${fileName} has been deleted.`)
+  } catch (err) {
+    console.error(err)
+  }
 }
-
-
 
 module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
